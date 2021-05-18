a4d4840d099f95f870196185fecd34bdb3393877
mgeaghan <michael.p.geaghan@gmail.com>
2021-05-18 20:03:28 +1000

    functional map editor

--- js/raycaster.js
+++ js/raycaster.js
@@ -2,8 +2,17 @@ class Raycaster {
 
 }
 
+// CONTROLS
+let draw_mode = document.getElementById("draw-mode");
+// END CONTROLS
+
+// MAP
 let rc_map = new RaycasterMap("raycaster-map-canvas", {
 	scale: 8,
 });
 rc_map.drawMap();
-rc_map.enableDrawing();
+rc_map.setDrawMode(draw_mode.checked, 2, true);
+draw_mode.onclick = () => {
+	rc_map.setDrawMode(draw_mode.checked, 2, true);
+}
+// END MAP
--- js/rc-map.js
+++ js/rc-map.js
@@ -19,15 +19,25 @@ class RaycasterMap {
 		this.rect = this.canvas.getBoundingClientRect();
 		this.ctx = this.canvas.getContext("2d");
 		this.map = this.initMap(this.width, this.height);
+		this.mapGhost = this.initMapGhost(this.width, this.height);
 		this.fillColour = props.hasOwnProperty("fillColour") ? props.fillColour : "#343332";
+		this.ghostFillColour = props.hasOwnProperty("ghostFillColour") ? props.ghostFillColour : "#34333231";
+		this.ghostRadius = props.hasOwnProperty("ghostRadius") ? props.ghostRadius : 2;
+		this.keepBorder = props.hasOwnProperty("keepBorder") ? props.keepBorder : true;
+		this.trackMouseDraw = this.trackMouse(this, this.ghostRadius, false, false, this.keepBorder);
+		this.trackMouseClear = this.trackMouse(this, this.ghostRadius, false, true, this.keepBorder);
+		this.trackMouseGhost = this.trackMouse(this, this.ghostRadius, true, false, this.keepBorder);
 		this.noFillColour = props.hasOwnProperty("noFillColour") ? props.noFillColour : "#d8dee1";
 		this.strokeColour = props.hasOwnProperty("strokeColour") ? props.strokeColour : "#c8ced1";
 		this.lastUpdate = undefined;
-		this.frameInterval = props.hasOwnProperty("frameInterval") ? props.frameInterval : 25;
+		this.frameInterval = props.hasOwnProperty("frameInterval") ? props.frameInterval : 1000;
+		this.canvas.oncontextmenu = (event) => {
+			event.preventDefault();
+		}
 	}
 
 	initMap(width, height) {
-		let map = []
+		let map = [];
 		for (let i = 0; i < height; i++) {
 			map.push([])
 			for (let j = 0; j < width; j++) {
@@ -39,6 +49,21 @@ class RaycasterMap {
 		return map;
 	}
 
+	initMapGhost(width, height) {
+		let map = [];
+		for (let i = 0; i < height; i++) {
+			map.push([])
+			for (let j = 0; j < width; j++) {
+				map[i].push(false);
+			}
+		}
+		return map;
+	}
+
+	clear() {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+
 	drawMap() {
 		for (let i = 0; i < this.height; i++) {
 			let rectStartY = i * this.scale;
@@ -46,6 +71,9 @@ class RaycasterMap {
 				let rectStartX = j * this.scale
 				if (this.map[i][j]) {
 					this.ctx.fillStyle = this.fillColour;
+					this.ctx.fillRect(rectStartX, rectStartY, this.scale, this.scale);
+				} else if (this.mapGhost[i][j]) {
+					this.ctx.fillStyle = this.ghostFillColour;
 					this.ctx.fillRect(rectStartX, rectStartY, this.scale, this.scale);
 				} else {
 					this.ctx.fillStyle = this.noFillColour;
@@ -57,19 +85,61 @@ class RaycasterMap {
 		}
 	}
 
-	fillSquare(x, y) {
-		this.map[y][x] = true;
+	fillSquare(x, y, radius=0, ghost=false, clear=false, keepBorder=true) {
+		let map = ghost ? this.mapGhost : this.map;
+		let r = Math.floor(Math.max(radius, 0));
+		if (r === 0) {
+			if (y == 0 || x == 0 || y == (this.height - 1) || x == (this.width - 1)) {
+				map[y][x] = keepBorder ? true : (clear ? false : true);
+			} else {
+				map[y][x] = clear ? false : true;
+			}
+		} else {
+			let xMin = Math.max(x - r, 0);
+			let xMax = Math.min(x + r, this.width - 1);
+			let yMin = Math.max(y - r, 0);
+			let yMax = Math.min(y + r, this.height - 1);
+			for (let i = yMin; yMin <= i && i <= yMax; i++) {
+				for (let j = xMin; xMin <= j && j <= xMax; j++) {
+					if (Math.sqrt(((i - y) ** 2) + ((j - x) ** 2)) <= r) {
+						if (i == 0 || j == 0 || i == (this.height - 1) || j == (this.width - 1)) {
+							map[i][j] = keepBorder ? true : (clear ? false : true);
+						} else {
+							map[i][j] = clear ? false : true;
+						}
+					}
+				}
+			}
+		}
 	}
 
-	clearSquare(x, y) {
-		this.map[y][x] = false;
+	clearMapGhost() {
+		this.mapGhost = this.initMapGhost(this.width, this.height);
 	}
 
-	trackMouse(self) {
+	getXYPos(self, event) {
+		let xPos = Math.floor((event.clientX - self.rect.left) / self.scale);
+		let yPos = Math.floor((event.clientY - self.rect.top) / self.scale);
+		if (xPos < 0) {
+			xPos = 0;
+		} else if (xPos >= self.width) {
+			xPos = self.width - 1;
+		}
+		if (yPos < 0) {
+			yPos = 0;
+		} else if (yPos >= self.height) {
+			yPos = self.height - 1;
+		}
+		return [xPos, yPos];
+	}
+
+	trackMouse(self, radius=0, ghost=false, clear=false, keepBorder=true) {
 		return (event) => {
-			let xPos = Math.floor((event.clientX - self.rect.left) / self.scale);
-			let yPos = Math.floor((event.clientY - self.rect.top) / self.scale);
-			self.fillSquare(xPos, yPos);
+			let xyPos = self.getXYPos(self, event);
+			let xPos = xyPos[0];
+			let yPos = xyPos[1];
+			self.clearMapGhost();
+			self.fillSquare(xPos, yPos, radius, ghost, clear, keepBorder);
 		}
 	}
 
@@ -79,20 +149,47 @@ class RaycasterMap {
 		}
 		const elapsed = timestamp - this.lastUpdate;
 		if (elapsed > this.frameInterval) {
+			this.clear();
 			this.drawMap();
 		}
 		requestAnimationFrame(timestamp => this.drawLoop(timestamp));
 	}
 
-	enableDrawing() {
-		let trackMouse = this.trackMouse(this);
-		this.canvas.addEventListener("mousedown", (event) => {
-			trackMouse(event);
-			this.canvas.addEventListener("mousemove", trackMouse);
-		});
-		this.canvas.addEventListener("mouseup", () => {
-			this.canvas.removeEventListener("mousemove", trackMouse);
-		});
+	setDrawMode(enabled=false) {
+		if (enabled) {
+			this.canvas.addEventListener("mousemove", this.trackMouseGhost);
+			this.canvas.addEventListener("mousedown", (event) => {
+				// this.canvas.removeEventListener("mousemove", trackMouseGhost);
+				let clear = false;
+				if (event.button !== 0) {
+					clear = true;
+				}
+				if (clear) {
+					this.trackMouseClear(event);
+					this.canvas.addEventListener("mousemove", this.trackMouseClear);
+				} else {
+					this.trackMouseDraw(event);
+					this.canvas.addEventListener("mousemove", this.trackMouseDraw);
+				}
+			});
+			this.canvas.addEventListener("mouseup", (event) => {
+				let clear = false;
+				if (event.button !== 0) {
+					clear = true;
+				}
+				if (clear) {
+					this.canvas.removeEventListener("mousemove", this.trackMouseClear);
+				} else {
+					this.canvas.removeEventListener("mousemove", this.trackMouseDraw);
+				}
+				// this.canvas.addEventListener("mousemove", trackMouseGhost);
+			});
+		} else {
+			this.clearMapGhost();
+			this.canvas.removeEventListener("mousemove", this.trackMouseGhost);
+			this.canvas.removeEventListener("mousemove", this.trackMouseClear);
+			this.canvas.removeEventListener("mousemove", this.trackMouseDraw);
+		}
 		requestAnimationFrame(timestamp => this.drawLoop(timestamp));
 	}
 }
