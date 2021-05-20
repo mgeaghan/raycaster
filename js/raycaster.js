71f15210203d8e1cf4f940c352b0f7cbc50a7e24
mgeaghan <michael.p.geaghan@gmail.com>
2021-05-20 12:11:25 +1000

    controls

--- js/raycaster.js
+++ js/raycaster.js
@@ -4,6 +4,11 @@ class Raycaster {
 
 // CONTROLS
 let draw_mode = document.getElementById("draw-mode");
+let slider_fov = document.getElementById("slider-fov");
+let slider_dov = document.getElementById("slider-dov");
+let slider_nrays = document.getElementById("slider-nrays");
+let draw_collision = document.getElementById("draw-collision");
+let draw_rays = document.getElementById("draw-rays");
 // END CONTROLS
 
 // FPS
@@ -17,14 +22,20 @@ let rc_fps = new RaycasterFPS("raycaster-fps-canvas", {
 // MAP
 let rc_map = new RaycasterMap("raycaster-map-canvas", {
 	scale: 8,
-	drawCollisions: true,
-	drawRays: true,
+	drawCollisions: draw_collision.checked,
+	drawRays: draw_rays.checked,
 });
 rc_map.drawMap();
 rc_map.setDrawMode(draw_mode.checked);
 draw_mode.onclick = () => {
 	rc_map.setDrawMode(draw_mode.checked);
 }
+draw_collision.onclick = () => {
+	rc_map.drawCollisions = draw_collision.checked;
+};
+draw_rays.onclick = () => {
+	rc_map.drawRays = draw_rays.checked;
+};
 // END MAP
 
 // PLAYER
@@ -32,12 +43,21 @@ let player = new Player(8, 8, rc_map.width, rc_map.height, {
 	collisionNPoints: 4,
 	collisionRadius: 2,
 	map: rc_map.map,
-	fov: (Math.PI / 3),
-	nrays: 96,
-	dov: 150,
+	fov: slider_fov.value,
+	nrays: slider_nrays.value,
+	dov: slider_dov.value,
 });
 player.enableMovement();
 player.setCollisions(true);
+slider_fov.oninput = () => {
+	player.fov = slider_fov.value;
+};
+slider_dov.oninput = () => {
+	player.dov = slider_dov.value;
+};
+slider_nrays.oninput = () => {
+	player.nrays = slider_nrays.value;
+};
 // END PLAYER
 
 rc_map.setPlayer(player);
